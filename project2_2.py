9e5c7d3462c4a3bcec0073b80f1aab6d70f3c474
刘芳 <liufang@liufangdeMacBook-Pro.local>
2018-06-29 15:26:59 +0800

    modify

--- project2_2.py
+++ project2_2.py
@@ -1,10 +1,8 @@
 import sys
 import json
 from pyspark import SparkContext
-#hashmap
 
-##./bin/spark-submit --master local[8] --driver-memory 2G project2.py /Users/liufang/Documents/BitTiger-CS502-1802/data/ads_0502.txt
-##spark-submit --master local[8] --driver-memory 2G /Users/liufang/Documents/BitTiger-CS502-1802/week7/project2.py /Users/liufang/Documents/BitTiger-CS502-1802/data/ads_0502.txt
+
 def get_ad_pair(line):
     entry = json.loads(line.strip())
     return (entry['campaignId'],str(entry['bidPrice'])+'_'+str(entry['adId']))
@@ -24,15 +22,12 @@ def get_adId(line):
 
 
 if __name__ == "__main__":
-    #adfile = sys.argv[1] #raw ads data
-    adfile="/Users/liufang/Documents/BitTiger-CS502-1802/data/ads_0502.txt"
-    budgetfile="/Users/liufang/Documents/BitTiger-CS502-1802/data/budget.txt"
+    adfile = sys.argv[1] #raw ads data
+    budgetfile = sys.argv[2] #raw budgetfile data
     sc = SparkContext(appName="campaign_bidprice_avg")
     adlist=sc.textFile(adfile).map(get_ad_pair)
     bglist=sc.textFile(budgetfile).map(get_budget_pair)
     df = adlist.leftOuterJoin(bglist).distinct()
     df=df.map(lambda pair:(pair[1][1]/get_bidprice(pair[1][0]),get_adId(pair[1][0]))).sortBy(lambda pair:(pair[0],pair[1]),ascending=True).map(lambda pair:(pair[1],pair[0]))
-    #df=df.map(lambda pair:(pair[1][1]/get_bidprice(pair[1][0]),get_adId(pair[1][0]))).sortByKey(ascending=False).map(lambda pair:(pair[1],pair[0])
-    #df.saveAsTextFile("/Users/liufang/Documents/BitTiger-CS502-1802/data11")
-    print df.take(30)
+    df.saveAsTextFile("data11")
     sc.stop()
